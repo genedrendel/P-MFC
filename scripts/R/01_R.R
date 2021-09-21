04e6dc6322b9b1546e3e70cb46e2c05ca608dbc9
Gene Drendel <genedrendel@gmail.com>
2021-09-21 12:39:35 +1000

    CLAM fix

--- scripts/R/01_R.R
+++ scripts/R/01_R.R
@@ -2006,36 +2006,35 @@ clamtest(comm, groups, coverage.limit = 10, specialization = 2/3,  npoints = 20,
 #example
 
 # Example using two mite groups. The mite data are available in vegan
-data(mite)
-# Two mite species associations (Legendre 2005, Fig. 4)
-group.1 <- c(1,2,4:8,10:15,17,19:22,24,26:30)
-group.2 <- c(3,9,16,18,23,25,31:35)
-# Separate Hellinger transformations of the two groups of species
-mite.hel.1 <- decostand(mite[,group.1], "hel")
-mite.hel.2 <- decostand(mite[,group.2], "hel")
-rownames(mite.hel.1) = paste("S",1:nrow(mite),sep="")
-rownames(mite.hel.2) = paste("S",1:nrow(mite),sep="")
-out <- CCorA(mite.hel.1, mite.hel.2)
-out
-biplot(out, "ob") # Two plots of objects
-biplot(out, "v", cex=c(0.7,0.6)) # Two plots of variables
-biplot(out, "ov", cex=c(0.7,0.6)) # Four plots (2 for objects, 2 for variables)
-biplot(out, "b", cex=c(0.7,0.6)) # Two biplots
-biplot(out, xlabs = NA, plot.axes = c(3,5)) # Plot axes 3, 5. No object names
-biplot(out, plot.type="biplots", xlabs = NULL) # Replace object names by numbers
-# Example using random numbers. No significant relationship is expected
-mat1 <- matrix(rnorm(60),20,3)
-mat2 <- matrix(rnorm(100),20,5)
-out2 = CCorA(mat1, mat2, permutations=99)
-out2
-biplot(out2, "b")
 
 data(mite)
 data(mite.env)
-sol <- with(mite.env, clamtest(mite, Shrub=="None", alpha=0.005))
+sol <- with(mite.env, clamtest(mite, Shrub=="None", alpha=0.005)) # with Shrub==none , Spec_TRUE species fall on the lower right, and Spec_FLASE species fall on upper left of plot
+#Shrub==NONE in this instance results in NONE specialists being called Spec_TRUE, all others lumped as Spec_FALS
 summary(sol)
 head(sol)
-plot(sol)
+plot(sol, xlab="None", ylab="Other", position=NULL)
+
+#Try to adapt to my data....
+#first otu table must be transposed so that species are columns, samples = rows
+#doesnt seem possible with excel...exceeds maximum column #
+#transpose in R
+
+t_otu_table <- as.data.frame(t(otu_table))
+
+sol <- with(treat, clamtest(t_otu_table, Substrate=="Water", alpha=0.005))
+
+sol <- with(treat, clamtest(t_otu_table, Location=="Root", alpha=0.005))
+
+
+
+summary(sol)
+head(sol)
+plot(sol, position="topright", bty = "n", xpd=TRUE)
+plot(sol, xlab="Anode", ylab="Other", position=NULL, bty = "n", xpd=TRUE)
+plot(sol, xlab="Water", ylab="Soil")
+
+write.csv(sol,"File_.csv", row.names = TRUE)
 
 # Other WIP / General Useful script section -------------------------------
 
